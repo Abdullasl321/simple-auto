4c911b5e1e2ffa3f71d3d5bc898b00522f2f5610
Abdullasl321 <108709846+Abdullasl321@users.noreply.github.com>
2024-05-28 13:44:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,113 +1,25 @@
-const {
-    DisconnectReason,
-    useMultiFileAuthState,
-    Browsers,
-    Mimetype,
-} = require("@whiskeysockets/baileys");
+const { BaileysClass } = require("@bot-wa/bot-wa-baileys");
+const qri = require("qr-image");
 
-const pino = require("pino");
-const makeWASocket = require("@whiskeysockets/baileys").default;
-const qrimage = require("qr-image");
-const express = require("express");
-const path = require("path");
-const fs = require("fs");
+const botBaileys = new BaileysClass({});
 
-const app = express();
+botBaileys.on("auth_failure", async (error) =>
+    console.log("ERROR BOT: ", error),
+);
+botBaileys.on("qr", (qr) => {
+    //console.log("NEW QR CODE: ", qr);
 
-app.use(express.static(path.join(__dirname, "public")));
-
-app.get("/", (req, res) => {
-    res.send("This is the root route.");
+    // var svg_string = qri.imageSync(qr, { type: "png" });
+    var qr_svg = qri.image(qr, { type: "png" });
+    qr_svg.pipe(require("fs").createWriteStream("./public/qr.png"));
 });
+botBaileys.on("ready", async () => console.log("READY BOT"));
 
-// Define a closure to keep track of processed remote JIDs
-const processedJids = new Set();
+let awaitingResponse = false;
 
-// Function to retrieve remote JIDs from JSON file
-async function getRemoteJidsFromJsonFile(filePath, sock) {
-    const data = fs.readFileSync(filePath, "utf8");
-
-    // Read the contents of creds.json synchronously
-    const fileContents = fs.readFileSync(filePath);
-
-    // Access the processedHistoryMessages array
-    const processedMessages = jsonData.processedHistoryMessages;
-
-    // Iterate over each message object and retrieve the remoteJid
-    processedMessages.forEach((message) => {
-        const remoteJid = message.key.remoteJid;
-        processedJids.add(remoteJid);
-    });
-}
-
-async function connectionLogic() {
-    const { state, saveCreds } = await useMultiFileAuthState("public");
-
-    const sock = makeWASocket({
-        version: [2, 2323, 4],
-        printQRInTerminal: false,
-        auth: state,
-        browser: Browsers.ubuntu("Chrome"),
-        logger: pino({ level: "silent" }),
-    });
-
-    sock.ev.on("connection.update", async (update) => {
-        const { connection, lastDisconnect, qr } = update || {};
-
-        if (qr) {
-            // Write custom logic over here
-            var qr_img = qrimage.image(qr, { type: "png" });
-            qr_img.pipe(require("fs").createWriteStream("public/qr.png"));
-        }
-
-        if (connection === "close") {
-            const shouldReconnect =
-                lastDisconnect?.error?.output?.statusCode !=
-                DisconnectReason.loggedOut;
-
-            if (shouldReconnect) {
-                connectionLogic();
-            }
-        }
-    });
-
-    sock.ev.on("messages.upsert", (messages) => {
-        if (messages.messages.length > 0 && messages.messages[0].message) {
-            const messageContent = messages.messages[0].message.conversation;
-
-            if (
-                messageContent &&
-                !messages.messages[0].key.remoteJid.includes("-")
-            ) {
-                sock.sendMessage(senderRemoteJid, {
-                    text: `*Hello There!*\n Thank you for contacting me!, I will reply to you as soon as possible. 🙂`,
-                });
-            }
-
-            const groupMessageContent = messages.messages
-                .map((message) => {
-                    if (message.message.extendedTextMessage) {
-                        return message.message.extendedTextMessage.text;
-                    } else {
-                        return "";
-                    }
-                })
-                .filter((content) => content.trim() !== "");
-        }
-    });
-
-    sock.ev.on("creds.update", async () => {
-        // Save the creds
-        await saveCreds();
-
-        // Call function to retrieve remote Jids and send message
-        await getRemoteJidsFromJsonFile("public/creds.json", sock);
-    });
-}
-
-connectionLogic();
-// Start the server
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+botBaileys.on("message", async (message) => {
+    botBaileys.sendMessage(
+        message.from,
+        "*Hello There!* \n Thank you for contacting me!, I will reply to you as soon as possible. 🙂",
+    );
 });
